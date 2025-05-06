3217623dd2069dd120f0377e6dae42afe7367b10
felipearredondo08 <104810739+felipearredondo08@users.noreply.github.com>
2025-05-06 19:52:33 -0300

    Commit inicial
    todo el proyecto

---
+++ Assets/Scripts/Character/ControlCaminar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCaminar : MonoBehaviour
+{
+    public float speed = 5f; 
+    private Rigidbody2D rb; 
+    private Vector2 movement; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); 
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void FixedUpdate()
+    {
+        
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
